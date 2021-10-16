b9005cf3e995fd2686aebb59a7dce2a8d3edc826
Md Rakibul Islam <irakibul189@gmail.com>
2021-10-16 21:33:49 +0600

    wedget

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,62 +1,43 @@
 <?php
 /**
- * The Template for displaying all single products
+ * The template for displaying all single posts
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see         https://docs.woocommerce.com/document/template-structure/
- * @package     WooCommerce\Templates
- * @version     1.6.4
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+get_header();
+if ( is_active_sidebar( 'sidebar-1' ) ) {
+    $main = 'col-lg-8';
+    $sidebar = 'col-lg-4';
+} else {
+    $main = 'col-lg-12';
+    $sidebar = 'col-lg-12';
 }
+?>
+    <!-- blog area five start here  -->
+    <section class="single-blog-page section">
+        <div class="container">
+            <div class="row">
+                <div class="<?php echo esc_attr($main);?> html-element-fix">
+                    <?php if ( have_posts() ) :
 
-get_header( 'shop' ); ?>
+                        /* Start the Loop */
+                        while ( have_posts() ) : the_post();
 
-	<?php
-		/**
-		 * woocommerce_before_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
+                            get_template_part( 'template-parts/singlecontent');
 
-		<?php while ( have_posts() ) : ?>
-			<?php the_post(); ?>
+                        endwhile;
+                    else :
 
-			<?php wc_get_template_part( 'content', 'single-product' ); ?>
+                        get_template_part( 'template-parts/content', 'none' );
 
-		<?php endwhile; // end of the loop. ?>
+                    endif; ?>
+                </div>
+                <div class="<?php echo esc_attr($sidebar);?>">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
+        </div>
+    </section>
 
-	<?php
-		/**
-		 * woocommerce_after_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	?>
-
-	<?php
-		/**
-		 * woocommerce_sidebar hook.
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		do_action( 'woocommerce_sidebar' );
-	?>
-
-<?php
-get_footer( 'shop' );
-
-/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
+<?php get_footer();?>
